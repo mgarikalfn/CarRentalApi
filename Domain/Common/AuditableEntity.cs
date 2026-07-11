9437f06376bb2dc3b3c67ea9d8f0baa0dc0abaed
girum <girumkenenisa@gmail.com>
2026-07-11 16:51:39 +0300

    refactored the domain to support DDD

---
+++ Domain/Common/AuditableEntity.cs
@@ -0,0 +1,9 @@
+using Domain.Common;
+
+public abstract class AuditableEntity : Entity
+{
+    public DateTime CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModifiedAt { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
