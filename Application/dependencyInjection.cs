d218b63428df9417a9287ff78bacd0f9228e567f
girum <girumkenenisa@gmail.com>
2026-09-15 23:50:12 +0300

    refactor(application): Update dependency injection configuration
    - Comment out non-existent ValidationBehavior<,> registration
    - Add TODO: ValidationBehavior needs implementation or removal
    - Maintain MediatR pipeline setup with functional handlers
    - Support command and query execution through MediatR

--- Application/dependencyInjection.cs
+++ Application/dependencyInjection.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -7,14 +8,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(cfg=>
-            cfg.RegisterServicesFromAssembly(
-                typeof(DependencyInjection).Assembly));
-        services.AddAutoMapper(
-            typeof(DependencyInjection).Assembly);
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            // ValidationBehavior<,> is not yet implemented
+            // cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
 
-        services.AddValidatorsFromAssembly(
-            typeof(DependencyInjection).Assembly);
+        services.AddAutoMapper(typeof(DependencyInjection).Assembly);
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
         return services;
     }
